38cd20cd5d33e5d54a8dde7c22a5fea6fab1618e
Jhon E C Papamija <joepapamija@gmail.com>
2019-10-03 12:05:30 -0300

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const moongose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+moongose.connect('mongodb+srv://onmistack:onmistack@rocketseat-week-9-lgavj.mongodb.net/admin?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(cors());
+app.use(express.json());
+/* Fazer a foto aparecer quando acessar a url/files */
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+app.listen(3333);
